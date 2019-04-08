2aff9bf3c88e61a63b9c4336558125aa74538863
trevor-shepard <trevorbshepard@gmail.com>
2019-04-08 12:40:35 -0700

    Add session reducer/store

---
+++ src/actions/session_actions.js
@@ -0,0 +1,49 @@
+import * as SessionApiUtil from '../util/api_utils';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+export const CLEAR_SESSION_ERRORS = 'CLEAR_SESSION_ERRORS'
+
+// Action Creators
+export const receiveCurrentUser = (currentUser) => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+});
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+});
+
+export const receiveSessionErrors = (errors) =>({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
+
+export const clearSessionErrors = () => ({
+    type: CLEAR_SESSION_ERRORS
+})
+
+
+// Thunk Actions
+export const login = (user) => dispatch => (
+    SessionApiUtil.login(user)
+    .then(
+        (response) => dispatch(receiveCurrentUser(response)),
+        (response) => dispatch(receiveSessionErrors(response.responseJSON))
+        )
+);
+
+export const logout = () => dispatch => {
+    localStorage.removeItem('token');
+
+    return dispatch(logoutCurrentUser())
+};
+
+export const signup = (user) => dispatch => (
+    SessionApiUtil.signup(user)
+    .then(
+        (response) => dispatch(receiveCurrentUser(response)),
+        (response) => dispatch(receiveSessionErrors(response.responseJSON))
+    )
+);
--- src/util/api_utils.js
+++ src/util/api_utils.js
@@ -5,28 +5,31 @@ axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 
-export const handle_login = (data) => {
-  fetch('http://localhost:8000/token-auth/', {
+export const login = (data) => {
+  return fetch('http://localhost:8000/token-auth/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   })
-    .then(res => res.json())
 };
 
 
-export const handle_signup = (data) => {
-  fetch('http://localhost:8000/core/users/', {
+export const signup = (data) => {
+  return fetch('http://localhost:8000/core/users/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   })
-  .then(res => res.json())
 };
+
+export const logout = () => {
+  localStorage.removeItem('token');
+};
+
 
 export const request_user_stats = (days) => {
   const instance = axios.create({
@@ -38,8 +41,7 @@ export const request_user_stats = (days) => {
   return instance.get(url)
 }
 
-
-export const test = () => {
+export const request_history = () => {
   const instance = axios.create({
     baseURL: 'http://localhost:8000/',
     headers: {'Authorization': `JWT ${localStorage.getItem('token')}`}
