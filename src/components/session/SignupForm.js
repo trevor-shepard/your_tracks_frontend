74e6b4557168f99653d9144c7b4d713ee7b3f170
trevor-shepard <trevorbshepard@gmail.com>
2019-04-09 00:39:28 -0700

    Add sign up

--- src/components/session/SignupForm.js
+++ src/components/session/SignupForm.js
@@ -1,49 +1,15 @@
+import { connect } from 'react-redux';
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import { signup } from '../../actions/session_actions'
+import SessionForm from './SessionForm'
 
-class SignupForm extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
 
-  handle_change = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState(prevstate => {
-      const newState = { ...prevstate };
-      newState[name] = value;
-      return newState;
-    });
-  };
+const mSTP = (state) => ({
+  form_name: "Sign Up"
+});
 
-  render() {
-    return (
-      <form onSubmit={e => this.props.handle_signup(e, this.state)}>
-        <h4>Sign Up</h4>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handle_change}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handle_change}
-        />
-        <input type="submit" />
-      </form>
-    );
-  }
-}
+const mDTP = () =>  dispatch => ({
+  action: (user) => dispatch(signup(user))
+});
 
-export default SignupForm;
-
-SignupForm.propTypes = {
-  handle_signup: PropTypes.func.isRequired
-};
+export default connect(mSTP, mDTP)(SessionForm);
