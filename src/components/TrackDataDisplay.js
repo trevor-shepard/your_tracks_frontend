e85561f43c1635ac8ae6863d37905b27ad03fe7b
trevor-shepard <trevorbshepard@gmail.com>
2019-04-11 12:17:09 -0700

    Add classNames to TrackDataDisplay

--- src/components/TrackDataDisplay.js
+++ src/components/TrackDataDisplay.js
@@ -1,12 +1,15 @@
 import React from 'react';
+// song name, artist, playcount
+
+
 
 const TrackDataDisplay = ({track_data}) => {
     const title = track_data[0]
     const value = track_data[1]
     return (
-        <div>
-            <div>{title}</div>
-            <div>{value}</div>
+        <div className="track-display">
+            <div className="track-title">{title}</div>
+            <div className="play-count">{value}</div>
         </div>
     )
 };
